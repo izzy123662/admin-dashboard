871520a4a608ec7da0ea4835a21e70072f4eaca1
izzy123662 <dabekilaizabela@gmail.com>
2023-09-02 15:55:55 +0200

    Pie

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React from 'react';
+
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  AccumulationLegend,
+  PieSeries,
+  AccumulationDataLabel,
+  Inject,
+  AccumulationTooltip,
+} from '@syncfusion/ej2-react-charts';
+
+import { Header } from '../../components';
+
+import { pieChartData } from '../../data/dummy';
+
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const Pie = () => {
-  return <div>Pie</div>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header
+        category="Bar"
+        title="Olympic Medal Counts - RIO"
+      />
+      <ChartComponent
+        id="area-chart"
+        height="420px"
+        primaryXAxis={barPrimaryXAxis}
+        primaryYAxis={barPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === 'Dark' ? '#33373E' : '#FFFFFF'}>
+        <Inject
+          services={[DataLabel, Legend, Category, Tooltip, ColumnSeries]}
+        />
+        <SeriesCollectionDirective>
+          {barCustomSeries.map((item, index) => (
+            <SeriesDirective
+              key={index}
+              {...item}
+            />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
 };
 
 export default Pie;
